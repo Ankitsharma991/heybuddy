bdcff3959729f48aa9dc70ac9c4878d5b25d4992
ankitsharma991 <sharmaa7861@gmail.com>
2025-11-05 12:20:56 +0530

    wip: finish prisma setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
+
+const Page = async () => {
+  const users = await prisma.user.findMany();
+
   return (
-    <div className="text-red-500">
-      Hello World
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      {JSON.stringify(users)}
     </div>
   );
 }
+
+export default Page
